12d2a931e5523d4d6bde46252e326a10acc50be5
aoirint <aoirint@gmail.com>
2019-07-21 20:39:20 +0900

    impl: chara classification

---
+++ SSBUCharaClassifier.py
@@ -0,0 +1,79 @@
+from skimage.feature import hog
+import numpy as np
+import json
+
+class SSBUCharaClassifier:
+    def __init__(self, feature_json):
+        self.feature_json = feature_json
+
+        with open(feature_json, 'r') as fp:
+            data = json.load(fp)
+
+        chara_id2name = sorted(list(data.keys()))
+
+        charas = []
+        features = []
+        for chara_id, chara_name in enumerate(chara_id2name):
+            fts = data[chara_name]
+
+            # n = 0
+            for feature in fts:
+                charas.append(int(chara_id))
+                features.append(np.asarray(feature, dtype=np.float32))
+                # n += 1
+                # if n == 4:
+                #     break
+
+        self.categories = set(charas)
+        self.datacount = len(charas)
+
+        self.chara_id2name = chara_id2name
+        self.charas = np.asarray(charas, dtype=np.int32)
+        self.features = np.asarray(features, dtype=np.float32)
+
+
+    def __call__(self, img, k=3):
+        # img isinstance of np.ndarray
+        print(img.shape)
+        assert len(img.shape) == 2 # gray
+        assert img.shape[1] == 110 and img.shape[0] == 110
+        h0 = hog(img)
+
+        dists = np.linalg.norm(self.features - h0, axis=1)
+
+        sarg = np.argsort(dists) # sorted-arg
+        topKarg = sarg[:k]
+
+        charas = self.charas[topKarg].tolist()
+        names = []
+        for i in range(len(charas)):
+            chara_id = int(charas[i])
+            name = self.chara_id2name[chara_id]
+            names.append(name)
+
+        return names, dists[topKarg].tolist()
+
+if __name__ == '__main__':
+    import argparse
+    import time
+    import cv2
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('dictionary', type=str)
+    parser.add_argument('input', type=str)
+    args = parser.parse_args()
+
+    print('loading...')
+    classifier = SSBUCharaClassifier(feature_json=args.dictionary)
+    print('loaded')
+
+    img = cv2.imread(args.input, 0)
+
+    t = time.time()
+    # img = cv2.resize(img, (110, 110))
+
+    ret = classifier(img)
+    elapsed = time.time() - t
+
+    print(ret)
+    print('FPS: %f (%f s)' % (1/elapsed, elapsed, ))
--- SSBUFrameAnalyzer.py
+++ SSBUFrameAnalyzer.py
@@ -4,21 +4,24 @@ import json
 # import numpy as np
 from SSBUDigitClassifier import SSBUDigitClassifier
 from SSBUNameRecognizer import SSBUNameRecognizer
+from SSBUCharaClassifier import SSBUCharaClassifier
 
 class SSBUFrameAnalyzer:
-    def __init__(self, digit_classifier, name_recognizer):
+    def __init__(self, digit_classifier, name_recognizer, chara_classifier):
         self.digit_classifier = digit_classifier
         self.name_recognizer = name_recognizer
+        self.chara_classifier = chara_classifier
 
-    def __call__(self, frame):
-        fighter_num = 2
+    def __call__(self, frame, fighter_num=2):
 
         dmgs = self.analyze_damage(frame, fighter_num=fighter_num)
         names = self.analyze_name(frame, fighter_num=fighter_num)
+        charas = self.analyze_chara(frame, fighter_num=fighter_num)
 
         fighters = {}
         for fighter_idx in range(fighter_num):
             fighters[fighter_idx] = {
+                'chara_name': charas[fighter_idx],
                 'name': names[fighter_idx],
                 'damage': dmgs[fighter_idx],
             }
@@ -29,8 +32,70 @@ class SSBUFrameAnalyzer:
 
         return result
 
+    def fighters_info_bbox(self, fighter_num):
+        assert fighter_num in (2, 3, 4), 'Not implemented'
+
+        # FIXME: magic number
+        if fighter_num == 2:
+            return [
+                # Fighter 1
+                [ 245    ,560, 240,160 ],
+                # Fighter 2
+                [ 245+495,560, 240,160 ],
+            ]
+        elif fighter_num == 3:
+            return [
+                # Fighter 1
+                [ 75    ,560, 240,160 ],
+                # Fighter 2
+                [ 75+416,560, 240,160 ],
+                # Fighter 3
+                [ 75+832,560, 240,160 ],
+            ]
+        elif fighter_num == 4:
+            return [
+                # Fighter 1
+                [ 98    ,560, 240,160 ],
+                # Fighter 2
+                [ 98+272,560, 240,160 ],
+                # Fighter 3
+                [ 98+545,560, 240,160 ],
+                # Fighter 4
+                [ 98+817,560, 240,160 ],
+            ]
+    def fighters_damage_bboxes(self, fighter_num):
+        info_bboxes = self.fighters_info_bbox(fighter_num=fighter_num)
+
+        ret = []
+        for fighter_idx, bbox in enumerate(info_bboxes):
+            left = bbox[0] + 85
+            top = bbox[1] + 50
+
+            ret.append([
+                [ left   ,top, 35,55 ],
+                [ left+30,top, 35,55 ],
+                [ left+60,top, 35,55 ],
+                [ left+97,top+28, 18,25 ],
+            ])
+        return ret
+    def fighters_name_bbox(self, fighter_num):
+        info_bboxes = self.fighters_info_bbox(fighter_num=fighter_num)
+
+        ret = []
+        for fighter_idx, bbox in enumerate(info_bboxes):
+            ret.append([ bbox[0]+105,bbox[1]+110, 120,16 ])
+        return ret
+    def fighters_chara_bbox(self, fighter_num):
+        info_bboxes = self.fighters_info_bbox(fighter_num=fighter_num)
+
+        ret = []
+        for fighter_idx, bbox in enumerate(info_bboxes):
+            ret.append([ bbox[0]+10,bbox[1]+28, 110,110 ])
+        return ret
+
     def analyze_damage(self, frame, fighter_num):
-        assert fighter_num == 2, 'Not implemented'
+        fighters_dmg_bboxes = self.fighters_damage_bboxes(fighter_num=fighter_num)
+        assert fighter_num == len(fighters_dmg_bboxes)
 
         dc = self.digit_classifier
         def predict_digit(img):
@@ -42,26 +107,6 @@ class SSBUFrameAnalyzer:
             digit = digits[0] if min_dist < thresh_dist else None
             return digit
 
-        # 2P Battle Only
-        fighters_dmg_bboxes = [ # FIXME: magic number
-            # Fighter 1
-            [
-                [ 335   ,610   , 35,55 ],
-                [ 335+30,610   , 35,55 ],
-                [ 335+65,610   , 35,55 ],
-                [ 335+97,610+28, 18,25 ],
-            ],
-            # Fighter 2
-            [
-                [ 830   ,610   , 35,55 ],
-                [ 830+30,610   , 35,55 ],
-                [ 830+65,610   , 35,55 ],
-                [ 830+97,610+28, 18,25 ],
-            ],
-        ]
-
-        assert fighter_num == len(fighters_dmg_bboxes)
-
         result = {}
         for fighter_idx in range(fighter_num):
             bboxes = fighters_dmg_bboxes[fighter_idx]
@@ -72,7 +117,7 @@ class SSBUFrameAnalyzer:
                 dimg = cv2.cvtColor(dimg, cv2.COLOR_BGR2GRAY) # GRAY
                 dimg = cv2.resize(dimg, (35, 55)) # FIXME: magic number
 
-                # cv2.imwrite('fighter-%d-%d.png' % (fighter_idx, bbox_idx, ), dimg)
+                cv2.imwrite('fighter-dmg-%d-%d.png' % (fighter_idx, bbox_idx, ), dimg)
 
                 digit = predict_digit(dimg)
                 if digit is None:
@@ -91,19 +136,10 @@ class SSBUFrameAnalyzer:
         return result
 
     def analyze_name(self, frame, fighter_num):
-        assert fighter_num == 2, 'Not implemented'
+        fighters_name_bbox = self.fighters_name_bbox(fighter_num=fighter_num)
+        assert fighter_num == len(fighters_name_bbox)
 
         nr = self.name_recognizer
-
-        # 2P Battle Only
-        fighters_name_bbox = [ # FIXME: magic number
-            # Fighter 1
-            [ 350,670, 120,16 ],
-            # Fighter 2
-            [ 845,670, 120,16 ],
-        ]
-
-        assert fighter_num == len(fighters_name_bbox)
 
         result = {}
         for fighter_idx in range(fighter_num):
@@ -112,7 +148,38 @@ class SSBUFrameAnalyzer:
             nimg = frame[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]] # RGB
             nimg = cv2.cvtColor(nimg, cv2.COLOR_BGR2GRAY) # GRAY
 
+            cv2.imwrite('fighter-name-%d.png' % (fighter_idx, ), nimg)
+
             name = nr(nimg)
+            result[fighter_idx] = name
+
+        return result
+
+    def analyze_chara(self, frame, fighter_num):
+        fighters_chara_bbox = self.fighters_chara_bbox(fighter_num=fighter_num)
+        assert fighter_num == len(fighters_chara_bbox)
+
+        cc = self.chara_classifier
+        def predict_chara(img):
+            names, dists = cc(img, k=3)
+            min_dist = dists[0]
+
+            print(names, dists)
+            thresh_dist = 10.
+
+            name = names[0] if min_dist < thresh_dist else None
+            return name
+
+        result = {}
+        for fighter_idx in range(fighter_num):
+            bbox = fighters_chara_bbox[fighter_idx]
+
+            cimg = frame[bbox[1]:bbox[1]+bbox[3], bbox[0]:bbox[0]+bbox[2]] # RGB
+            cimg = cv2.cvtColor(cimg, cv2.COLOR_BGR2GRAY) # GRAY
+
+            cv2.imwrite('fighter-face-%d.png' % (fighter_idx, ), cimg)
+
+            name = predict_chara(cimg)
             result[fighter_idx] = name
 
         return result
@@ -126,7 +193,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('digit_dictionary', type=str)
+    parser.add_argument('chara_dictionary', type=str)
     parser.add_argument('input', type=str)
+    parser.add_argument('fighter_num', type=int) # TODO: predict
     args = parser.parse_args()
 
     print('loading digit classifier...')
@@ -137,7 +206,11 @@ if __name__ == '__main__':
     name_recognizer = SSBUNameRecognizer()
     print('loaded name recognizer')
 
-    analyzer = SSBUFrameAnalyzer(digit_classifier=digit_classifier, name_recognizer=name_recognizer)
+    print('loading chara classifier...')
+    chara_classifier = SSBUCharaClassifier(feature_json=args.chara_dictionary)
+    print('loaded chara classifier')
+
+    analyzer = SSBUFrameAnalyzer(digit_classifier=digit_classifier, name_recognizer=name_recognizer, chara_classifier=chara_classifier)
 
     frame = cv2.imread(args.input, 1) # RGB
     frame = cv2.resize(frame, (1280, 720))
@@ -147,7 +220,7 @@ if __name__ == '__main__':
 
     t = time.time()
 
-    ret = analyzer(frame)
+    ret = analyzer(frame, fighter_num=args.fighter_num)
     elapsed = time.time() - t
 
     print(ret)
